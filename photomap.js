6b740d53feabb998b75c624602f6dfdcd0e6b419
brookedimaggio204-ops <brookedimaggio204@gmail.com>
2026-04-27 19:11:59 -0500

    Update photomap.js

--- photomap.js
+++ photomap.js
@@ -20,9 +20,11 @@ function createPhotoMap () {
   var map = L.map('map').setView([29.97, -90.287], 11);
   
   // create basemap object. See examples at https://leaflet-extras.github.io/leaflet-providers/preview/
-var basemap = L.tileLayer('https://basemaps.arcgis.com/arcgis/rest/services/World_Basemap_v2/VectorTileServer', {
-    maxZoom: 11,
-    attribution: 'Esri"
+var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 0,
+	maxZoom: 11,
+	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'png'
 }).addTo(map);
 
   
