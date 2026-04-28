f243ef57a89fe0e9f2d7aea66172ac1bcc4ff73c
brookedimaggio204-ops <brookedimaggio204@gmail.com>
2026-04-27 19:02:44 -0500

    Update photomap.js

--- photomap.js
+++ photomap.js
@@ -22,7 +22,6 @@ function createPhotoMap () {
   // create basemap object. See examples at https://leaflet-extras.github.io/leaflet-providers/preview/
 var basemap = L.tileLayer('https://basemaps.arcgis.com/arcgis/rest/services/World_Basemap_v2/VectorTileServer', {
  maxZoom: 11,
- attribution: 'USGS'
 }).addTo(map);
 
   
